7e204080eaa9c9c4f417a32ca15e4c9c0ab0cd2d
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-03-07 21:12:30 +0100

    setup project

---
+++ src/test/java/com/example/airlinebackend/AirlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
